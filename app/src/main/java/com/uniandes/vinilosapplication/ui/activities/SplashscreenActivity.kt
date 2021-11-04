5377263a4857d25c040bd9907b32a3da51f9777b
lulodigitallatam <lulodigitallatam@gmail.com>
2021-11-04 14:17:52 -0500

    Agregar fuentes

--- app/src/main/java/com/uniandes/vinilosapplication/ui/activities/SplashscreenActivity.kt
+++ app/src/main/java/com/uniandes/vinilosapplication/ui/activities/SplashscreenActivity.kt
@@ -1,12 +1,22 @@
 package com.uniandes.vinilosapplication.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.uniandes.vinilosapplication.MainActivity
 import com.uniandes.vinilosapplication.R
 
 class SplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+    }
+
+    fun onClick(v: View) {
+        if(v?.id == R.id.bt_start) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish();
+        }
     }
 }
