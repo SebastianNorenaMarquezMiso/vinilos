583846ed54ece5d159c77a4e05a1bf3b81367324
Zully Alarcón Rodriguez <z.alarcon@uniandes.edu.co>
2021-11-06 20:11:05 -0500

    Ajustes interfaces (#21)
    Co-authored-by: lulodigitallatam <lulodigitallatam@gmail.com>

--- app/src/androidTest/java/com/uniandes/vinilosapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/uniandes/vinilosapplication/ExampleInstrumentedTest.kt
@@ -1,11 +1,10 @@
 package com.uniandes.vinilosapplication
 
-import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
 
 /**
@@ -13,12 +12,14 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+
+// @RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4ClassRunner::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val appContext = InstrumentationRegistry.getInstrumentation().getTargetContext()
         assertEquals("com.uniandes.vinilosapplication", appContext.packageName)
     }
 }
