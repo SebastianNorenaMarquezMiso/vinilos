f3f0f2d27a63c3db5e0122ec51d44ae708dedd30
javargas1098 <ja.vargas1098@gmail.com>
2021-11-06 19:40:02 -0500

    Primeras pruebas

---
+++ app/src/androidTest/java/com/uniandes/vinilosapplication/ui/activities/SplashscreenActivityTest2.kt
@@ -0,0 +1,89 @@
+package com.uniandes.vinilosapplication.ui.activities
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import com.uniandes.vinilosapplication.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class SplashscreenActivityTest2 {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(SplashscreenActivity::class.java)
+
+    @Test
+    fun splashscreenActivityTest2() {
+        val appCompatButton = onView(
+            allOf(
+                withId(R.id.bt_start), withText("Iniciar"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                        1
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatButton.perform(click())
+
+        val button = onView(
+            allOf(
+                withText("NUEVO ALBUM"),
+                withParent(withParent(withId(R.id.nav_host_fragment))),
+                isDisplayed()
+            )
+        )
+        button.check(matches(isDisplayed()))
+
+        val recyclerView = onView(
+            allOf(
+                withId(R.id.fragments_rv),
+                withParent(withParent(IsInstanceOf.instanceOf(android.widget.FrameLayout::class.java))),
+                isDisplayed()
+            )
+        )
+        recyclerView.check(matches(isDisplayed()))
+
+        val confirmLoginBtn = onView(allOf(withId(R.id.iv_cover_album)) )
+        confirmLoginBtn.perform(scrollTo(), click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
