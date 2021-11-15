68f05da7bae7631360e91c67d99e000d830b4bea
Jimmy <jmmy.cardenas@avaldigitallabs.com>
2021-11-14 23:12:13 -0500

    Add artist and collector tests

---
+++ app/src/androidTest/java/com/uniandes/vinilosapplication/ui/activities/ArtistScreenTest.kt
@@ -0,0 +1,91 @@
+package com.uniandes.vinilosapplication.ui.activities
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import com.uniandes.vinilosapplication.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class ArtistScreenTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(SplashscreenActivity::class.java)
+
+    @Test
+    fun artistScreenTest() {
+        val appCompatButton = onView(
+            allOf(
+                withId(R.id.bt_start), withText("Iniciar"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.layout_splashScreen),
+                        1
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatButton.perform(click())
+
+        Thread.sleep(2000)
+
+        val bottomNavigationItemView = onView(
+            allOf(
+                withId(R.id.musicianFragment), withContentDescription("ARTISTAS"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.bottom_navigatin_view),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView.perform(click())
+
+        val textView = onView(
+            allOf(
+                withId(R.id.textView6), withText("Rubén Blades Bellido de Luna"),
+                withParent(withParent(withId(R.id.fragments_rv))),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(isDisplayed()))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
